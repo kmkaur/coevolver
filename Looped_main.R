c79716442aafe0e9d2ecab2db82915ecad03bc3c
Katrina Kaur <kaur@zoology.ubc.ca>
2018-02-27 15:29:04 -0800

    cleaning up code and creating new biotic selection functions

--- Looped_main.R
+++ Looped_main.R
@@ -5,7 +5,7 @@ trial_pops = get_starting_pop(pars=test_pars)
 meta_i <- trial_pops$meta_i
 meta_j <- trial_pops$meta_j
 
-#mating and reproduction
+######mating and reproduction#####
 newgen_i <- list()
 for(i in 1:50){
   
@@ -20,7 +20,7 @@ for(i in 1:50){
   newgen_j[i] <- trial_pop_mr
 }
 
-#migration
+#####migration#####
 remix_i <- list()
 for(i in 1:50){
   
@@ -33,7 +33,7 @@ for(i in 1:50){
   remix_j <- migrate_m(newgen_j, round(test_pars$m_j[i]*test_pars$K_j[i]))
 } 
 
-#abiotic sel
+#####abiotic sel#####
 post_sel_i <- list()
 for(i in 1:50){
   post_sel <- abiotic_sel(remix_i[i], test_pars$theta_i[i], test_pars$gamma_i[i])
@@ -46,39 +46,75 @@ for(i in 1:50){
   post_sel_j[i] <- post_sel
 }
 
+#####match individuals#####
 partners <- lapply(seq_len(length(meta_i)), function(x)
   get_partners(post_sel_i[[x]], post_sel_j[[x]]))
 
-diff_i <- list()
-diff_j <- list()
+#####differences in trait values#####
 diff <- list()
 for(i in 1:50){
-  d <- lapply(partners[[i]]$part, function(x) {(x[1] - x[2])})
-  diff_i[i] <- d$sp_i
-  diff_j[i] <- d$sp_j
+  d <- partners[[i]]$part$sp_i-partners[[i]]$part$sp_j
+  diff[[i]] <- d
 }
 
-diff <- cbind(diff_i,diff_j)
+#####biotic selection#####
 
+#first fix the fitness_f_match fxn:
+  #change zeta to zeta_i[a] and alpha to alpha_i[a] and 'a' needs to go from 1:50
+  #change diff to diff[[a]] and 'a' needs to go from 1:50
+  #length of partners[[a]]$part is nrow(partners[[a]]$part)
+  #define length_part <- nrow(partners[[a]]$part)
+  #diff[[a]][b] 'b' needs to go from 1:length_part in partners[[a]]$part
+  #so overall, diff[[a]][1:length_part], a goes from 1:50
 
-
-
-
-
-
-
-
-
-
-Listkxm <- as.list(test_pars$K_i*test_pars$m_i)
-as.numeric(Listkxm[1])
-
-
-mylist2 <- list()
-
-for(i in 1:2){
+#fitness_f_match <- function(diff, sp, pars){ 
+    #decide what goes into the function
+    fit_match <- list()
+    w1 <- list()
+  for(i in 1:50){
+    length_part <- nrow(partners[[i]]$part)
+    diff_1 <- diff[[i]][1:length_part]
+    fit_match[[i]] <- diff_1
+    #if (pars$int[sp] == "cost"){
+        #define what pars$int is, currently NULL
+    w <- -test_pars$zeta_i[i] * exp(-test_pars$alpha_i[i] * fit_match[[i]][1:length_part]^2)
+    #} else {
+    w <- test_pars$zeta_i[i] * exp(-test_pars$alpha_i[i] * fit_match[[i]][1:length_part]^2)
+    w1[[i]] <- w
+  }
+#}  
   
-trial_test_migrate_i <- migrate_m(pops_i[i], round(pars_test$m_i*pars_test$K_i))
-}
+#fitness_f_diff <- function(diff, sp, pars){
+    #decide what goes into the function
+    fit_diff <- list()
+    w2 <- list()
+  for(i in 1:50){
+    length_part <- nrow(partners[[i]]$part)
+    diff_1 <- diff[[i]][1:length_part]
+    fit_diff[[i]] <- diff_1
+    #if (pars$int[sp] == "cost"){ 
+     #define what pars$int is, currently NULL
+    w <- -test_pars$zeta_i[i] / (1 + test_pars$alpha_i[i] * fit_match[[i]][1:length_part]^2)
+    #} else {
+    w <- test_pars$zeta_i[i] / (1 + test_pars$alpha_i[i] * fit_match[[i]][1:length_part]^2)
+    w2[[i]] <- w
+  }
+ #}
+  
+#biotic_sel <- function(x, diff, sp, pars){
+#      if (pars$type == "match"){ #something buggy here, always is 0
+#       w  <- fitness_f_match(diff, sp, pars)
+#      } else {
+#        w <- fitness_f_diff(diff, sp, pars)
+#      }
+#      get_survivors(x, w)
+#    }
 
-trial_test_sel_i <- abiotic_sel(trial_test_migrate_i, pars$theta_i[1], pars$gamma_i[1])
+#####add remainders#####
+    #need to define a function first
+
+
+
+    
+   
+
