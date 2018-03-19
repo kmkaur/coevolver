1db4e0817a73c219ecdd9dfec6c8b81a7fb5b2b0
Katrina Kaur <kaur@zoology.ubc.ca>
2018-03-18 17:35:09 -0700

    worked on biotic selection functions

--- Looped_main.R
+++ Looped_main.R
@@ -1,4 +1,4 @@
-
+#####create populations#####
 trial_pops <- build_starting_pop(pars=yoder_defaults())
 
 meta_i <- trial_pops$pops$meta_i
@@ -7,14 +7,12 @@ meta_j <- trial_pops$pops$meta_j
 ######mating and reproduction#####
 newgen_i <- list()
 for(i in 1:50){
-  
   trial_pop_mr <- mate_repro(meta_i[i], trial_pops$pars$K_i[i], trial_pops$pars$v_s)
   newgen_i[i] <- trial_pop_mr
 }
 
 newgen_j <- list()
 for(i in 1:50){
-  
   trial_pop_mr <- mate_repro(meta_j[i], trial_pops$pars$K_j[i], trial_pops$pars$v_s)
   newgen_j[i] <- trial_pop_mr
 }
@@ -22,13 +20,11 @@ for(i in 1:50){
 #####migration#####
 remix_i <- list()
 for(i in 1:50){
-  
   remix_i <- migrate_m(newgen_i, round(trial_pops$pars$m_i[i]*trial_pops$pars$K_i[i]))
 }
 
 remix_j <- list()
 for(i in 1:50){
-  
   remix_j <- migrate_m(newgen_j, round(trial_pops$pars$m_j[i]*trial_pops$pars$K_j[i]))
 } 
 
@@ -64,40 +60,89 @@ for(i in 1:50){
   diff_1 <- diff[[i]][1:length_part]
   fit_diff[[i]] <- diff_1}
 
-fitness_matching_output <- list()
+fit_match_cost_i <- list()
 for(i in 1:50){
-  fitness_matching_out <- fitness_f_match(test_pars$zeta_i[i], test_pars$alpha_i[i], fit_diff[[i]])
-  fitness_matching_output[[i]] <- fitness_matching_out
+  fitness_matching_cost <- fitness_f_match_cost(test_pars$zeta_i[i], test_pars$alpha_i[i], fit_diff[[i]])
+  fit_match_cost_i[[i]] <- fitness_matching_cost
+}
+
+fit_match_cost_j <- list()
+for(i in 1:50){
+  fitness_matching_cost <- fitness_f_match_cost(test_pars$zeta_j[i], test_pars$alpha_j[i], fit_diff[[i]])
+  fit_match_cost_j[[i]] <- fitness_matching_cost
+}
+
+fit_match_ben_i <- list()
+for(i in 1:50){
+  fitness_matching_ben <- fitness_f_match_ben(test_pars$zeta_i[i], test_pars$alpha_i[i], fit_diff[[i]])
+  fit_match_cost_i[[i]] <- fitness_matching_ben
+}
+
+fit_match_ben_j <- list()
+for(i in 1:50){
+  fitness_matching_ben <- fitness_f_match_ben(test_pars$zeta_j[i], test_pars$alpha_j[i], fit_diff[[i]])
+  fit_match_cost_j[[i]] <- fitness_matching_ben
 }
 
 #####fitness differences#####
-fit_diff <- list()
-w1 <- list()
+fit_diff_cost_i <- list()
 for(i in 1:50){
-  length_part <- nrow(partners[[i]]$part)
-  diff_1 <- diff[[i]][1:length_part]
-  fit_diff[[i]] <- diff_1}
-
-fitness_diff_output <- list()
-for(i in 1:50){
-  fitness_diff_out <- fitness_f_diff(test_pars$zeta_i[i], test_pars$alpha_i[i], fit_diff[[i]])
-  fitness_diff_output[[i]] <- fitness_diff_out
+  fitness_difference_cost <- fitness_f_diff_cost(test_pars$zeta_i[i], test_pars$alpha_i[i], fit_diff[[i]])
+  fit_diff_cost_i[[i]] <- fitness_difference_cost
 }
-  
-#biotic_sel <- function(zeta, alpha, fit_match, fit_diff){
-#      if (pars$type == "match"){ #something buggy here, always is 0, define pars$type
-#       w  <- fitness_f_match(zeta, alpha, fit_match)
-#      } else {
-#       w <- fitness_f_diff(zeta, alpha, fit_diff)
-#      }
-#      get_survivors(x, w)
-      
-#    }
+
+fit_diff_cost_j <- list()
+for(i in 1:50){
+  fitness_difference_cost <- fitness_f_diff_cost(test_pars$zeta_j[i], test_pars$alpha_j[i], fit_diff[[i]])
+  fit_diff_cost_j[[i]] <- fitness_difference_cost
+}
+
+fit_diff_ben_i <- list()
+for(i in 1:50){
+  fitness_difference_ben <- fitness_f_diff_ben(test_pars$zeta_i[i], test_pars$alpha_i[i], fit_diff[[i]])
+  fit_diff_ben_i[[i]] <- fitness_difference_ben
+}
+
+fit_diff_ben_j <- list()
+for(i in 1:50){
+  fitness_difference_ben <- fitness_f_diff_ben(test_pars$zeta_j[i], test_pars$alpha_j[i], fit_diff[[i]])
+  fit_diff_ben_j[[i]] <- fitness_difference_ben
+}
+
+###survivors and remainders###
+surv_m_c_i <- list()
+for(i in 1:50){
+  survivors <- get_survivors(post_sel_i[i], fit_match_cost_i[[i]])
+  surv_m_c_i[[i]] <- survivors
+}
+
+surv_m_c_j <- list()
+for(i in 1:50){
+  survivors <- get_survivors(post_sel_j[i], fit_match_cost_j[[i]])
+  surv_m_c_j[[i]] <- survivors
+}
+
+surv_m_b_i
+surv_m_b_j
+surv_d_c_i
+surv_d_c_j
+surv_d_b_i
+surv_d_b_j
 
 #####add remainders#####
-    #get remainders that survived from partners and add them here
+post_bio_i <- list()
+for(i in 1:50){
+post_bio <- c(unlist(surv_m_c_i[[i]]), partners[[i]]$rem_i)
+post_bio_i[[i]] <- post_bio[!is.na(post_bio)]
+}
 
-
+post_bio_j <- list()
+for(i in 1:50){
+  post_bio <- c(unlist(surv_m_c_j[[i]]), partners[[i]]$rem_j)
+  post_bio_j[[i]] <- post_bio[!is.na(post_bio)]
+}
+    
+i_and_j <- list(post_bio_i, post_bio_j)
 
 #####alternate way of getting partners and difference#####
 #not looped yet
--- R/biotic-int-func.R
+++ R/biotic-int-func.R
@@ -24,111 +24,25 @@ get_partners <- function(i,j){
        rem_i=rem_i, rem_j=rem_j) #fixing part, it now shows up
 }
 
-
-## Function for fitness matching
-#need to figure out cost vs benefit
-fitness_f_match <- function(zeta, alpha, fit_diff){ 
-  #if (interaction == "cost"){
+## Functions for fitness matching
+fitness_f_match_cost <- function(zeta, alpha, fit_diff){ 
   w <- -zeta * exp(-alpha * fit_diff^2)
-  #} else {
+  out <- list(w)
+}
+
+fitness_f_match_ben <- function(zeta, alpha, fit_diff){ 
   w <- zeta * exp(-alpha * fit_diff^2)
   out <- list(w)
 }
 
-## Function for fitness difference
-#need to figure out cost vs benefit
-fitness_f_diff <- function(zeta, alpha, fit_diff){ 
-  #if (interaction == "cost"){
-  w <- -pars$zeta[sp] / (1 + pars$alpha[sp] * fit_diff^2)
-  #} else {
-  w <- pars$zeta[sp] / (1 + pars$alpha[sp] * fit_diff^2)
+## Functions for fitness difference
+fitness_f_diff_cost <- function(zeta, alpha, fit_diff){ 
+  w <- -zeta / (1 + alpha * fit_diff^2)
   out <- list(w)
 }
 
-## Functions for evaluating fitnesses of partners
-#I have started to re do this function
-biotic_sel <- function(x, zeta, alpha, fit_diff){
-
-    #if (pars$type == "match"){
-         w  <- fitness_f_match(zeta, alpha, dit_diff)
-    # } else {
-         w <- fitness_f_diff(zeta, alpha, fit_diff)
-     #}
-    get_survivors(x, w)
+fitness_f_diff_ben <- function(zeta, alpha, fit_diff){ 
+  w <- zeta / (1 + alpha * fit_diff^2)
+  out <- list(w)
 }
-
-
-
-
-
-
-####################
-
-#first fix the fitness_f_match fxn:
-#change zeta to zeta_i[a] and alpha to alpha_i[a] and 'a' needs to go from 1:50
-#change diff to diff[[a]] and 'a' needs to go from 1:50
-#length of partners[[a]]$part is nrow(partners[[a]]$part)
-#define length_part <- nrow(partners[[a]]$part)
-#diff[[a]][b] 'b' needs to go from 1:length_part in partners[[a]]$part
-#so overall, diff[[a]][1:length_part], a goes from 1:50
-
-#V2
-#fitness_f_match <- function(diff, sp, pars){ 
-#pars = pars, stays the same
-#diff = diff, stays the same
-#what would sp be?
-fit_match <- list()
-w1 <- list()
-for(i in 1:50){
-  length_part <- nrow(partners[[i]]$part)
-  diff_1 <- diff[[i]][1:length_part]
-  fit_match[[i]] <- diff_1
-  #if (pars$int[sp] == "cost"){
-  #define what pars$int is, currently NULL
-  w <- pars$zeta_i[i] * exp(-pars$alpha_i[i] * fit_match[[i]][1:length_part]^2)
-  #} else {
-  w <- pars$zeta_i[i] * exp(-pars$alpha_i[i] * fit_match[[i]][1:length_part]^2)
-  w1[[i]] <- w
-}
-#}  
-
-#fitness_f_diff <- function(diff, sp, pars){
-#decide what goes into the function
-fit_diff <- list()
-w2 <- list()
-for(i in 1:50){
-  length_part <- nrow(partners[[i]]$part)
-  diff_1 <- diff[[i]][1:length_part]
-  fit_diff[[i]] <- diff_1
-  #if (pars$int[sp] == "cost"){ 
-  #define what pars$int is, currently NULL
-  w <- -pars$zeta_i[i] / (1 + pars$alpha_i[i] * fit_diff[[i]][1:length_part]^2)
-  #} else {
-  w <- pars$zeta_i[i] / (1 + pars$alpha_i[i] * fit_diff[[i]][1:length_part]^2)
-  w2[[i]] <- w
-}
-#}
-
-#V1
-## Matching fitness
-#I have started to re do this function
-fitness_f_match <- function(diff, sp, pars){
-  if (pars$int[sp] == "cost"){ #something buggy here, always is 0
-    w <- -pars$zeta[sp] * exp(-pars$alpha[sp] * diff^2)
-  } else {
-    w <-  pars$zeta[sp] * exp(-pars$alpha[sp] * diff^2)
-  }
-  w
-}
-
-## Difference fitness
-#I have started to re do this function
-fitness_f_diff <- function(diff, sp, pars){
-  if (pars$int[sp] == "cost"){ #something buggy here, always is 0
-    w <- -pars$zeta[sp] / (1 + pars$alpha[sp] * diff^2)
-  } else {
-    w <- pars$zeta[sp] / (1 + pars$alpha[sp] * diff^2)
-  }
-}
-
 
