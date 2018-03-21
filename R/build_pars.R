c5bcb01f2ab5778593ef0def33541b97b27db655
Katrina Kaur <kaur@zoology.ubc.ca>
2018-03-21 14:59:37 -0700

    added correlation structure to theta

--- R/build_pars.R
+++ R/build_pars.R
@@ -1,19 +1,11 @@
-## Functions for drawing values
-## N.B. Need to add correlation structure among optimum
+#load require libraries
 library(MASS)
 set.seed(123)
-N <- 50
-mean <- c(runif(1, 0.4, 0.6), runif(1, 0.4, 0.6))
-var <- runif(1, 0, 1)
-sigma1 <- runif(1, -.1, .1)
-sigma2 <- runif(1, -.1, .1)
-sigma <- matrix(c(sigma1^2, sigma1*sigma2*var, sigma1*sigma2*var, sigma2^2),2)
-theta <- mvrnorm(n=N, mu=mean, Sigma=sigma, empirical = FALSE)
-theta_i_cor <- theta[,1]
-theta_j_cor <- theta[,2]
-                     
-#by separating the values for i and j, it should fix the problem
-#of having the same carrying capacity per species for all 50 populations 
+   
+#####These are functions for starting values#####
+
+#by separating the values for i and j, it should fix the problem of 
+#having the same carrying capacity per species for all 50 populations 
 get_parameters <- function(f){
     N <- f$N()
     K_i <- round(f$K(N))
@@ -26,9 +18,8 @@ get_parameters <- function(f){
     zeta_j <- f$zeta(N) 
     m_i <- f$m(N) 
     m_j <- f$m(N)
-    #names(m) <- names(zeta) <- names(alpha) <- names(gamma) <- names(K) <- c("i", "j")
-    theta_i <- f$theta[,1] 
-    theta_j <- f$theta[,2]
+    theta_i <- f$theta(N)[,1]
+    theta_j <- f$theta(N)[,2]
     v_s <- f$v_s()
 
     list(N=N, K_i=K_i, K_j=K_j, gamma_i=gamma_i, gamma_j=gamma_j, 
@@ -37,8 +28,7 @@ get_parameters <- function(f){
 }
 
 
-## Defaults (Yoder and Nuismer)
-
+#Defaults parameters (Yoder & Nuismer, 2010)
 yoder_defaults <- function(){
     N <- function() 50
     K <- function(n) runif(n, 300, 2000)
@@ -46,10 +36,13 @@ yoder_defaults <- function(){
     alpha <- function(n) runif(n, 1,10)
     zeta <- function(n) runif(n, 0.01, 5)
     m <- function(n) runif(n, 0, 0.1)
-    #theta <- function(n) rnorm(n, runif(1, 0.4, 0.6), runif(1, 0, 1))
-    mu <- c(runif(1, 0.4, 0.6), runif(1, 0.4, 0.6))
-    sigma <- matrix(runif(50,-0.1,1)*runif(50,0,1), nrow=2, ncol = 2)
-    theta <- mvrnorm(n=50, mu, sigma, empirical = FALSE)
+    mean <- c(runif(1, 0.4, 0.6), runif(1, 0.4, 0.6))
+    var <- runif(1, 0, 1)
+    sigma1 <- runif(1, -.1, .1)
+    sigma2 <- runif(1, -.1, .1)
+    sigma <- matrix(c(sigma1^2, sigma1*sigma2*var, sigma1*sigma2*var, sigma2^2),2)
+    #sigma gives correlation structure to theta
+    theta <- function(n) mvrnorm(n, mu=mean, Sigma=sigma, empirical = FALSE)
     v_s <- function() 0.01
     list(N=N, K=K, gamma=gamma, alpha=alpha, zeta=zeta, m=m,
          theta=theta, v_s=v_s)
@@ -68,8 +61,7 @@ test_parameters <- function(){
        theta=theta, v_s=v_s)
 }
 
-## Build starting values
-#this works correctly now
+#####Build starting populations#####
 get_starting_pop <- function(pars){
     mi <- lapply(c(1:pars$N), function(x) {rnorm(pars$K_i[x],
                                                  pars$theta_i[x], 0.05)})
