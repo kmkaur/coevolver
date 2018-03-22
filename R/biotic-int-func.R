f0611f1353b8dac742923f133f24ef374afe1832
Katrina Kaur <kaur@zoology.ubc.ca>
2018-03-21 19:59:48 -0700

    commenting functions

--- R/biotic-int-func.R
+++ R/biotic-int-func.R
@@ -1,5 +1,4 @@
-## Match up individuals
-#this works now
+#####Function to match up individuals (partnerships)#####
 get_partners <- function(i,j){ 
   min_n <- min(length(i), length(j))
   ind_sp_i <- sample(seq_len(length(i)), min_n)
@@ -8,7 +7,7 @@ get_partners <- function(i,j){
   sp_j <- j[ind_sp_j]
   part <- data.frame(sp_i=sp_i, sp_j=sp_j)
   
-  ## leftovers
+  ## these are the leftovers
   if (length(i) > length(j)){
     rem_i <- i[-ind_sp_i]
     rem_j <- NA
@@ -21,28 +20,38 @@ get_partners <- function(i,j){
   }
   
   list(part = part, ind_sp_i=ind_sp_i, ind_sp_j=ind_sp_j,
-       rem_i=rem_i, rem_j=rem_j) #fixing part, it now shows up
+       rem_i=rem_i, rem_j=rem_j) 
 }
 
-## Functions for fitness matching
+#####Functions for fitness matching#####
+
+#costly interaction
 fitness_f_match_cost <- function(zeta, alpha, fit_diff){ 
   w <- -zeta * exp(-alpha * fit_diff^2)
   out <- list(w)
 }
 
+#beneficial interaction
 fitness_f_match_ben <- function(zeta, alpha, fit_diff){ 
   w <- zeta * exp(-alpha * fit_diff^2)
   out <- list(w)
 }
 
-## Functions for fitness difference
+#####Functions for fitness differences#####
+
+#costly interaction
 fitness_f_diff_cost <- function(zeta, alpha, fit_diff){ 
   w <- -zeta / (1 + alpha * fit_diff^2)
   out <- list(w)
 }
 
+#beneficial interaction
 fitness_f_diff_ben <- function(zeta, alpha, fit_diff){ 
   w <- zeta / (1 + alpha * fit_diff^2)
   out <- list(w)
 }
+
+#for mutualists, use both beneficial eq'ns
+#for competitors, use both costly eq'ns
+#for antagonists, use a beneficial eq'n for partner i and a costly eq'n for j
 
