08b757f5d7f3c70b5e93bdcec0ff1a105011b6ec
Katrina Kaur <kaur@zoology.ubc.ca>
2018-03-29 12:14:21 -0700

    add if statement to migration fxn

--- R/all_functions.R
+++ R/all_functions.R
@@ -94,7 +94,9 @@ mate_repro_k <- function(x, k, v_s)
 migrate_m <- function(x, z, km){
   pool <- sapply(x, function(y) y[c(1:km)])
   mkk <- sample(seq_len(length(pool)), size=km)
-  z[c(1:km)] <- pool[mkk]
+  if(km > 0) {
+  z[c(1:km)] <- pool[mkk] 
+  }
   z
 }
     
