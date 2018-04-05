03409ec71f935917c421a95fa4715eb01c84fc88
Katrina Kaur <kaur@zoology.ubc.ca>
2018-04-05 11:48:17 -0700

    changing 1:50 to 1:pars$N

--- R/Matching_mutualism_int.R
+++ R/Matching_mutualism_int.R
@@ -12,13 +12,13 @@ coev_div_single_gen <- function(meta_i, meta_j, pars){
 
 ptm <- proc.time()
 newgen_i <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   trial_pop_mr <- mate_repro(meta_i[i], pars$K_i[i], pars$v_s)
   newgen_i[i] <- trial_pop_mr
 }
 
 newgen_j <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   trial_pop_mr <- mate_repro(meta_j[i], pars$K_j[i], pars$v_s)
   newgen_j[i] <- trial_pop_mr
 }
@@ -28,13 +28,13 @@ proc.time()-ptm
 
 ptm <- proc.time()
 remix_i <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   remix <- migrate_m(newgen_i[i], newgen_i[[i]], round(pars$m_i[i]*pars$K_i[i]))
   remix_i[[i]] <- remix
 }
 
 remix_j <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   remix <- migrate_m(newgen_j[i], newgen_j[[i]], round(pars$m_j[i]*pars$K_j[i]))
   remix_j[[i]] <- remix
 }
@@ -44,13 +44,13 @@ proc.time()-ptm
 
 ptm <- proc.time()
 post_sel_i <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   post_sel <- abiotic_sel(remix_i[i], pars$theta_i[i], pars$gamma_i[i])
   post_sel_i[i] <- post_sel
 }
 
 post_sel_j <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   post_sel <- abiotic_sel(remix_j[i], pars$theta_j[i], pars$gamma_j[i])
   post_sel_j[i] <- post_sel
 }
@@ -60,7 +60,7 @@ proc.time()-ptm
 
 ptm <- proc.time()
 partners <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   gp <- get_partners(post_sel_i[[i]], post_sel_j[[i]])
   partners[[i]] <- gp
 }
@@ -70,7 +70,7 @@ proc.time()-ptm
 
 ptm <- proc.time()
 diff <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   d <- partners[[i]]$part$sp_i-partners[[i]]$part$sp_j
   diff[[i]] <- d
 }
@@ -81,19 +81,19 @@ proc.time()-ptm
 ptm <- proc.time()
 fit_diff <- list()
 w1 <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   length_part <- nrow(partners[[i]]$part)
   diff_1 <- diff[[i]][1:length_part]
   fit_diff[[i]] <- diff_1}
 
 fit_match_ben_i <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   fitness_matching_ben <- fitness_f_match_ben(pars$zeta_i[i], pars$alpha_i[i], fit_diff[[i]])
   fit_match_ben_i[[i]] <- fitness_matching_ben
 }
 
 fit_match_ben_j <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   fitness_matching_ben <- fitness_f_match_ben(pars$zeta_j[i], pars$alpha_j[i], fit_diff[[i]])
   fit_match_ben_j[[i]] <- fitness_matching_ben
 }
@@ -103,13 +103,13 @@ proc.time()-ptm
 
 ptm <- proc.time()
 surv_match_ben_i <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   survivors <- get_survivors(post_sel_i[i], fit_match_ben_i[[i]])
   surv_match_ben_i[[i]] <- survivors
 }
 
 surv_match_ben_j <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   survivors <- get_survivors(post_sel_j[i], fit_match_ben_j[[i]])
   surv_match_ben_j[[i]] <- survivors
 }
@@ -119,13 +119,13 @@ proc.time()-ptm
 
 ptm <- proc.time()
 post_bio_i <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   post_bio <- c(unlist(surv_match_ben_i[[i]]), partners[[i]]$rem_i)
   post_bio_i[[i]] <- post_bio[!is.na(post_bio)]
 }
 
 post_bio_j <- list()
-for(i in 1:50){
+for(i in 1:pars$N){
   post_bio <- c(unlist(surv_match_ben_j[[i]]), partners[[i]]$rem_j)
   post_bio_j[[i]] <- post_bio[!is.na(post_bio)]
 }
