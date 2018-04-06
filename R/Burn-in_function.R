3d464d884e085c4301cbc4f88a0ecc8cb3144b5c
Katrina Kaur <kaur@zoology.ubc.ca>
2018-04-05 22:58:03 -0700

    run simulation for n.gen

--- R/Burn-in_function.R
+++ R/Burn-in_function.R
@@ -6,7 +6,6 @@ trial_pops <- build_starting_pop(pars=yoder_2_pop())
 pars <- trial_pops$pars
 meta_i <- trial_pops$pops$meta_i
 meta_j <- trial_pops$pops$meta_j
-#out <- coev_div_single_gen(meta_i, meta_j, pars)
 
 coev_div <- function(pars, n.gen, burnin=FALSE, burnin.gen=200, print=FALSE){
   
@@ -44,25 +43,49 @@ coev_div <- function(pars, n.gen, burnin=FALSE, burnin.gen=200, print=FALSE){
     }
   }
   
-  for (j in 1:n.gen){
-  #need to define j
-  #need to find the mean of each population under each generation?
-  res_i <- c(0, sapply(meta_i, mean))
-  res_j <- c(0, sapply(meta_j, mean))
-  res   <- rbind(res_i, res_j)
-  names(res) <- c("gen", sapply(seq_len(length(meta_i)), function(x)
-    paste("pop", x, sep="_")))
-  }
+  ##run the simulation for the remaining gen (n.gen <- burnin.gen- n.gen)
+  #final burnin.gen value is the value we want for the remaining generations
+    pars$alpha_i <- alpha_i_burn[,burnin.gen] 
+    pars$alpha_j <- alpha_j_burn[,burnin.gen] 
+    pars$gamma_i <- gamma_i_burn[,burnin.gen]
+    pars$gamma_j <- gamma_j_burn[,burnin.gen]
+    
+    meta_i_list2 <- list()
+    meta_j_list2 <- list()
+    for (q in seq_len(n.gen)){
+      out <- coev_div_single_gen(meta_i, meta_j, pars)
+      meta_i <- out$pop_i
+      meta_j <- out$pop_j
+      meta_i_list2[[q]] <- meta_i
+      meta_j_list2[[q]] <- meta_j
+    }
+    
+    #NOW, meta_i_list and meta_j_list have the generations 1:burnin.gen
+    #AND, meta_i_list2 and meta_j_list2 have the generations n.gen (n.gen-burnin.gen)
+    
+    #need to combine both lists
+    
+    #need to calculate means and variances
   
-  for (i in seq_len(n.gen)){
-  #need to define i
+    for (j in 1:n.gen){
+    #need to define j
+    #need to find the mean of each population under each generation?
+    res_i <- c(0, sapply(meta_i_list[[j]], mean))
+    res_j <- c(0, sapply(meta_j_list[[j]], mean))
+    res   <- rbind(res_i, res_j)
+    names(res) <- c("gen", sapply(seq_len(length(meta_i)), function(x)
+    paste("pop", x, sep="_")))
+    }
+  
+    for (i in seq_len(n.gen)){
+    #need to define i
     out <- coev_div_single_gen(meta_i, meta_j, pars)
     meta_i <- out$pop_i
     meta_j <- out$pop_j
     res_i <- c(i, sapply(meta_i, mean))
     res_j <- c(i, sapply(meta_j, mean))
-  }
-  
+    }
+    
 }        
 
 
