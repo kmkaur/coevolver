ac74e11f26aed5a9b85b767fa385f7eaaeecd4a4
Katrina Kaur <kaur@zoology.ubc.ca>
2018-04-09 20:52:21 -0700

    calculate means and variances of simulations

--- R/Burn-in_function.R
+++ R/Burn-in_function.R
@@ -2,6 +2,8 @@
 #STILL working on this one!
 
 #build starting pars
+require(gmodels)
+require(ggplot2)
 trial_pops <- build_starting_pop(pars=yoder_2_pop())
 pars <- trial_pops$pars
 meta_i <- trial_pops$pops$meta_i
@@ -63,34 +65,64 @@ coev_div <- function(pars, n.gen, burnin=FALSE, burnin.gen=200, print=FALSE){
     #NOW, meta_i_list and meta_j_list have the generations 1:burnin.gen
     #AND, meta_i_list2 and meta_j_list2 have the generations n.gen (n.gen-burnin.gen)
     
-    #combine both lists
-    all_gens <- c(meta_i_list, meta_i_list2)
+    #combine lists so entire output has total gen information
+    all_gens_i <- c(meta_i_list, meta_i_list2)
+    all_gens_j <- c(meta_j_list, meta_j_list2)
     
-    #need to calculate means and variances
+    #need to calculate 
+    #1) x = generation #  and y = mean phenotype for i and for j and 95% CI
   
-    for (j in 1:n.gen){
-    #need to define j
-    #need to find the mean of each population under each generation?
-    res_i <- c(0, sapply(meta_i_list[[j]], mean))
-    res_j <- c(0, sapply(meta_j_list[[j]], mean))
-    res   <- rbind(res_i, res_j)
-    names(res) <- c("gen", sapply(seq_len(length(meta_i)), function(x)
-    paste("pop", x, sep="_")))
-    }
-  
-    for (i in seq_len(n.gen)){
-    #need to define i
-    out <- coev_div_single_gen(meta_i, meta_j, pars)
-    meta_i <- out$pop_i
-    meta_j <- out$pop_j
-    res_i <- c(i, sapply(meta_i, mean))
-    res_j <- c(i, sapply(meta_j, mean))
-    }
+    pop_means <- matrix(ncol=length(all_gens), nrow=pars$N)
+    pop_ci <- matrix(ncol=length(all_gens), nrow=pars$N)
+    pop_var <- matrix(ncol=length(all_gens), nrow=pars$N)
+    #columns=gen# and rows=pop#
+ 
+    for(q in 1:length(all_gens)){
+        for(r in 1:pars$N){
+          pop_means[r,q] <- mean(all_gens[[q]][[r]])
+          #pop_ci[r,q] <- ci(all_gens[[q]][[r]])
+          pop_var[r,q] <- var(all_gens[[q]][[r]])
+        }
+    #pop_means <- as.data.frame(pop_means)
+    #pop_var <- as.data.frame(pop_var)
     
-}        
+   }
+  
+    
+}
+
+#make final variance figure
+final_var <- as.data.frame(pop_var[,5])
+colnames(final_var) <- c("variance")
+var_graph <- qplot(final_var$variance, geom = "histogram", binwidth = 0.02, 
+                   xlab = "Final Variance", ylab = "Simulations")
 
 
 
+
+#     for (j in 1:n.gen){
+#       #need to define j
+#       #need to find the mean of each population under each generation?
+#       res_i <- c(0, sapply(meta_i_list[[j]], mean))
+#       res_j <- c(0, sapply(meta_j_list[[j]], mean))
+#       res   <- rbind(res_i, res_j)
+#       names(res) <- c("gen", sapply(seq_len(length(meta_i)), function(x)
+#         paste("pop", x, sep="_")))
+#     }
+#     
+#     for (i in seq_len(n.gen)){
+#     #need to define i
+#     out <- coev_div_single_gen(meta_i, meta_j, pars)
+#     meta_i <- out$pop_i
+#     meta_j <- out$pop_j
+#     res_i <- c(i, sapply(meta_i, mean))
+#     res_j <- c(i, sapply(meta_j, mean))
+#     }
+#     
+# }        
+# 
+# 
+# 
 
 
 
