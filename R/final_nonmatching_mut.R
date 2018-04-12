e2b1584013bbc386ead8c552ca96400d36327f77
Katrina Kaur <kaur@zoology.ubc.ca>
2018-04-12 13:44:29 -0700

    fix output

--- R/final_nonmatching_mut.R
+++ R/final_nonmatching_mut.R
@@ -596,7 +596,7 @@ coev_div_2 <- function(all_pars=NULL, n.gen, burnin=FALSE, burnin.gen, print=FAL
 solo_sim_nm <- coev_div_2(all_pars = NULL, n.gen = 1000, burnin = TRUE, burnin.gen = 200, print = FALSE)
 write.csv(solo_sim_mm, file = "solo_sim_nm.csv")
 
-#run the part 4 1000 times
+#run the part 3 1000 times
 mclapply(c(1:1000), function(x) coev_div_wrapper(x, n.gen = 1000, burnin = TRUE, burnin.gen = 200, 
                                                  print = FALSE), mc.cores = 50)
 
