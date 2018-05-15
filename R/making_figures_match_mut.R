00e4f228bcd2741b195e7a24041e1b93c2619f9d
Katrina Kaur <kaur@zoology.ubc.ca>
2018-05-15 12:48:51 -0700

    remove one loop

--- R/making_figures_match_mut.R
+++ R/making_figures_match_mut.R
@@ -3,31 +3,27 @@ library(ggplot2)
 library(cowplot)
 
 #making data structure work for figure 3 eqiuvalent
-setwd("/Users/Katrina/git_practice/coevolver/out_m_m")
+setwd("~/simulation_output/out_mut_match")
+
 list.filenames<-list.files(pattern=".rds")
-list.data.i<-list()
-list.data.j<-list()
+list.data <-list()
 
 #read rds files
 for (i in 1:length(list.filenames)){
-  list.data.i[[i]]<-readRDS(list.filenames[i])
-}
-
-for (i in 1:length(list.filenames)){
-  list.data.j[[i]]<-readRDS(list.filenames[i])
+  list.data[[i]]<-readRDS(list.filenames[i])
 }
 
 #find mean end variance
 end_variances_i <- list()
-for(i in 1:length(list.data.i)){
-  var_i <- as.data.frame(list.data.i[[i]]$pop_var_i)
+for(i in 1:length(list.data)){
+  var_i <- as.data.frame(list.data[[i]]$pop_var_i)
   end_var <- mean(var_i[,1000], na.rm=TRUE)
   end_variances_i[[i]] <- end_var
 }
 
 end_variances_j <- list()
-for(i in 1:length(list.data.j)){
-  var_i <- as.data.frame(list.data.j[[i]]$pop_var_j)
+for(i in 1:length(list.data)){
+  var_i <- as.data.frame(list.data[[i]]$pop_var_j)
   end_var <- mean(var_i[,1000], na.rm=TRUE)
   end_variances_j[[i]] <- end_var
 }
